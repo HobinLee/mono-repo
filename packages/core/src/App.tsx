036a901ca18b8381aeccd61aedd249f0e71fe241
Hobin Lee <viny5120@gmail.com>
2022-02-13 15:11:41 +0900

    feat: package 분리

--- packages/core/src/App.tsx
+++ packages/core/src/App.tsx
@@ -1,6 +1,6 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement } from "react";
 
-import Icon from '../public/assets/icon.svg';
+import Icon from "../public/assets/icon.svg";
 
 interface Props {}
 
